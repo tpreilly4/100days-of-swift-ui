b89cf8f978be977b45ecc83e789cea2cf85b377e
Tom Reilly <tpreilly4@gmail.com>
2024-02-07 20:09:24 -0500

    Day 27 - More Core ML, some UI building refreshes!

--- BetterRest/BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/BetterRest/ContentView.swift
@@ -6,32 +6,83 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+    
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 0.0
+    @State private var wakeUp = defaultWakeTime
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
-        VStack {
+        NavigationStack {
             Form {
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 1...24, step: 0.25)
-                DatePicker("Please enter a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
-                    .datePickerStyle(.graphical)
-                    .labelsHidden()
-                
-                Text(wakeUp, format: .dateTime.day().month().year())
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...16, step: 0.25)
+                }
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 0...20)
+                }
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message : {
+                Text(alertMessage)
             }
         }
-        .padding()
     }
-}
-
-func datesAndStuff () {
-    var components = DateComponents()
-    components.hour = 8
-    components.minute = 0
-    let date = Calendar.current.date(from: components)
+    
+    func calculateBedtime(){
+        do {
+            // Instantiate model
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            // Convert date to Doubles because thats what the ML model wants (number of seconds)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            // Let the model predict how much sleep you will get
+            let prediction = try model.prediction(wake: Int64(hour + minute), estimatedSleep: sleepAmount, coffee: Int64(coffeeAmount))
+            
+            // Subtract predicted amount of sleep from time they want to wake up
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            // Show the user via alert dialog
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
+    }
 }
 
 #Preview {
