1d0c00704776cac937eee3d1d78bfc44688e2647
Tom Reilly <tpreilly4@gmail.com>
2024-02-19 17:15:15 -0500

    Day 40 - Moonshot part 2
    More Codable, some Generics, styling everything

---
+++ Moonshot/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Tom Reilly on 2/18/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/Moonshot/MoonshotHome.swift
@@ -0,0 +1,62 @@
+//
+//  MoonshotHome.swift
+//  Moonshot
+//
+//  Created by Tom Reilly on 2/18/24.
+//
+
+import SwiftUI
+
+struct MoonshotHome: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let gridLayout = [
+        GridItem(.adaptive(minimum: 150)),
+    ]
+    var body: some View {
+        NavigationStack {
+            ScrollView{
+                LazyVGrid(columns: gridLayout) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Details")
+                        } label : {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack{
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground))
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+        }
+    }
+}
+
+#Preview {
+    MoonshotHome()
+}
