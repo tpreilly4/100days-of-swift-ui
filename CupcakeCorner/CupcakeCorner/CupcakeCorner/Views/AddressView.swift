3c785e13db906460a703e069a21aff35e5a4ee0d
Tom Reilly <tpreilly4@gmail.com>
2024-03-25 19:26:44 -0400

    Day 51 - Finishing CupcakeCorner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCorner/Views/AddressView.swift
@@ -0,0 +1,36 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Tom Reilly on 3/4/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @Bindable var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+
+            Section {
+                NavigationLink("Check out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(!order.hasValidAddress)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
