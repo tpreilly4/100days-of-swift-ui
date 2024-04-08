a5586561447a6527d59043f2f150be24d9d980af
Tom Reilly <tpreilly4@gmail.com>
2024-04-08 16:53:20 -0400

    Day 58 (part 1) - Queries and Relationships
    Saving the CloudKit part of Day 58 for a separate commit

--- SwiftDataProject/SwiftDataProject/Data/User.swift
+++ SwiftDataProject/SwiftDataProject/Data/User.swift
@@ -13,7 +13,8 @@ class User {
     var name: String
     var city: String
     var joinDate: Date
-
+    @Relationship(deleteRule: .cascade) var jobs = [Job]()
+    
     init(name: String, city: String, joinDate: Date) {
         self.name = name
         self.city = city
--- SwiftDataProject/SwiftDataProject/Views/ContentView.swift
+++ SwiftDataProject/SwiftDataProject/Views/ContentView.swift
@@ -10,17 +10,18 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(filter: #Predicate<User> { user in
-        user.name.localizedStandardContains("R") &&
-        user.city == "London"
-    }, sort: \User.name) var users: [User]
+    @Query(sort: \User.name) var users: [User]
+    
     @State private var path = [User]()
+    @State private var showingUpcomingOnly = false
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate),
+    ]
     
     var body: some View {
         NavigationStack {
-            List(users) { user in
-                Text(user.name)
-            }
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
             .navigationTitle("Users")
             .toolbar {
                 Button("Add Samples", systemImage: "plus") {
@@ -34,6 +35,24 @@ struct ContentView: View {
                     modelContext.insert(second)
                     modelContext.insert(third)
                     modelContext.insert(fourth)
+                }
+                Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                    showingUpcomingOnly.toggle()
+                }
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by Name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                SortDescriptor(\User.joinDate),
+                            ])
+                        
+                        Text("Sort by Join Date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name)
+                            ])
+                    }
                 }
             }
         }
